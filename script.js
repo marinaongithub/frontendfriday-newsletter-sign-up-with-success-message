c3493a13161684b60167b9c59eb9a1f9c7b07f26
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2024-03-31 18:22:45 +0100

    Added email validation + updated font

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Switch image from mobile to desktop and vice versa
+
 function switchImage(x) {
     var image = document.getElementById("sign-up-image");
     if (x.matches) { // If media query matches
@@ -13,13 +15,60 @@ function switchImage(x) {
     }
   }
   
-  // Create a MediaQueryList object
-  var x = window.matchMedia("(min-width: 426px)")
+    // Create a MediaQueryList object
+const x = window.matchMedia("(min-width: 426px)")
   
-  // Call listener function at run time
-  switchImage(x);
+    // Call listener function at run time
+switchImage(x);
   
-  // Attach listener function on state changes
-  x.addEventListener("change", function() {
+    // Attach listener function on state changes
+x.addEventListener("change", function() {
     switchImage(x);
-  });
+});
+
+
+
+// Custom validation for email field
+
+const form = document.getElementById('form');
+const emailInput = document.getElementById('email');
+
+// Check validity of email input
+function isValidEmail(email) {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    regex.test(email);
+}
+
+// Define display of error message
+function setError() {
+    const invalidMsg = document.getElementById('invalidMsg');
+    invalidMsg.style.visibility = "visible";
+    form.setAttribute('disabled', true);
+    emailInput.setAttribute('class', "invalid");
+}
+
+//Full email validation fonction
+function validateEmail() {
+    const emailValue = emailInput.value.trim();
+    if (!isValidEmail(emailValue)) {
+        setError();
+    }
+    else {
+        localStorage("email", emailValue);
+    }
+}
+
+//Removes error email display when input is changed
+emailInput.addEventListener('input', function() {
+        invalidMsg.style.visibility = "hidden";
+        emailInput.removeAttribute('class');
+    })
+    
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    validateEmail();
+});
+
+
+
